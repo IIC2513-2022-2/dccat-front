49d8983b27e4be1ec77b6f3363610a913e5e2d80
barbaraim <birarrazaval1@uc.cl>
2022-10-05 20:56:52 -0300

    feat: add useState and useEffect in Grid

--- dccat/src/components/Grid.js
+++ dccat/src/components/Grid.js
@@ -1,9 +1,49 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Tile from "./Tile";
 
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 function Grid(props) {
+  const [symbols, setSymbols] = useState(["", "", "", "", "", "", "", "", ""]);
+
+  // Temporales
+  const playerId = 0;
+  const matchId = 0;
+
+  // Llamada asincrona a la API
+  const getData = async () => {
+    const { data } = await axios.get(`${SERVER_URL}/matches/${matchId}/players/${playerId}`);
+    return data;
+  };
+
+
+  useEffect(()=>{
+    getData().then((data) => {
+      console.log(data);
+  
+      const newList = [...symbols];
+  
+      data["0"].map((mov) => {
+        const index = mov[0] * 3 + mov[1];
+        newList[index] = "O";
+        return 0;
+      });
+  
+      data["1"].map((mov) => {
+        const index = mov[0] * 3 + mov[1];
+        newList[index] = "X";
+        return 0;
+      }); 
+  
+      setSymbols(newList);
+      
+    });
+  }, [])
+
   return (
     <div className="Grid">
-      {props.listSymbols.map((symbol, index) => (
+      {symbols.map((symbol, index) => (
         <Tile id={`tile-${index}`} key={index} symbol={symbol} />
       ))}
     </div>
